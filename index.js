c5228b2aaec77b03bb91f568b45f068ef14a144c
Md Sakib Rahman <sakibrahman.user@gmail.com>
2025-11-10 02:01:09 +0600

    added user routes to store user data to DB with token verification

--- index.js
+++ index.js
@@ -35,6 +35,39 @@ async function run() {
     const movies = moviedb.collection('movies');
     const users = moviedb.collection('users');
 
+
+//-------------------- Firebase Admin
+
+var admin = require("firebase-admin");
+
+
+var serviceAccount = require("./movie-master-pro-49200-firebase-adminsdk.json");
+
+
+admin.initializeApp({
+
+  credential: admin.credential.cert(serviceAccount)
+
+});
+
+async function verifyToken(req, res, next) { // Firebase Admin Middleware for token verification
+  const authHeader = req.headers.authorization;
+  
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized Access: No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1]; // 
+  
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken; 
+    next();
+  } catch (error) {
+    return res.status(403).send({ message: 'Unauthorized Access: Invalid token' });
+  }
+}
+
 // -------------------- Get Api's
 
     app.get('/movies', async (req, res)=>{   // get all movies
@@ -47,7 +80,7 @@ async function run() {
         if(sort=='top_rated'){
           movieData = await movies.find().sort({rating: -1}).limit(5).toArray()
         }else if(sort=='latest'){
-          movieData = await movies.find().sort({ releaseYear: -1 }).limit(5).toArray()
+          movieData = await movies.find().sort({ releaseYear: -1 }).limit(6).toArray()
         }else if(sort=='movie_count'){
           movieData = await movies.find().toArray()
           movieData=movieData.length
@@ -58,7 +91,7 @@ async function run() {
         res.send(movieData)
     })
     app.get('/movies/:id', async (req, res)=>{ // get all movies by ID
-        const id = req.params;
+        const id = req.params.id;
         const query = { _id : new ObjectId(id) }
         console.log('\n\n\n /movies/id was hit !!!!\n\n\n')
         const movieData = await movies.findOne(query)
@@ -71,8 +104,18 @@ async function run() {
         res.send(usersData.length)
     })
 
-
-
+// -------------------- Post Api's
+app.post('/users', verifyToken, async (req, res) => {
+      console.log('/users was hit')
+      const user = req.body;
+      const existingUser = await users.findOne({ email: user.email });
+      if (existingUser) {
+        return res.send({ message: 'User already exists' });
+      }
+      
+      const result = await users.insertOne(user);
+      res.send(result);
+    });
 
   } finally {
     
